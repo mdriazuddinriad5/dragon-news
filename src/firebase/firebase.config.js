f7f75d36acf80479142240aed8545af5666079c1
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-04 10:29:08 +0600

    Environment setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNp06MpI-fM2cMq-K2n08pMDqYBfzV-Uc",
+  authDomain: "react-dragon-news-auth-e7917.firebaseapp.com",
+  projectId: "react-dragon-news-auth-e7917",
+  storageBucket: "react-dragon-news-auth-e7917.appspot.com",
+  messagingSenderId: "138183679960",
+  appId: "1:138183679960:web:9f41ce0abd9382c50b1cad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

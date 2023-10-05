59daa610414018f95c3933c0546802824536eec7
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-05 12:17:31 +0600

    Register and Login Page set up

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import NavBar from "../shared/NavBar/NavBar";
+
+const Login = () => {
+
+    const handleLogin= e=>{
+        e.preventDefault();
+        const form= new FormData(e.currentTarget);
+        const email= form.get('email');
+        const password= form.get('password')
+
+        console.log(email, password);
+    }
+
+    return (
+
+        <div>
+
+            <NavBar></NavBar>
+
+            <h2 className="text-2xl text-center my-10">Please Login</h2>
+
+            <form onSubmit={handleLogin} className="lg:w-1/2 w-3/4 mx-auto">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
+
+            <p className="text-center mt-4">Don&apos;t have an account? <Link to={'/register'} className="text-blue-700 font-bold">Register</Link></p>
+
+
+
+
+        </div>
+
+    );
+};
+
+export default Login;
